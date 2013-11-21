999d37dd8f7243c8ae10fe9e9dd2d32c817b71bb
Thasso Griebel <thasso.griebel@googlemail.com>
2013-11-22 00:20:04 +0100

    another rtd

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
-import os
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+try:
+    from setuptools import setup, Extension
+except:
+    from distribute_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, Extension
 
 name = 'pyjip'
 version = '0.3'
@@ -14,63 +18,29 @@ except:
     readme = ''
 
 
-def default_install():
-    try:
-        from setuptools import setup, Extension
-    except:
-        from distribute_setup import use_setuptools
-        use_setuptools()
-        from setuptools import setup, Extension
-    dispatcher_ext = Extension('jip.dispatcher',
-                               ['jip/dispatcher/jip_binding.c',
-                                'jip/dispatcher/jip_dispatcher.c'])
-    setup(
-        name=name,
-        version=version,
-        description=description,
-        author_email=author_email,
-        url=url,
-        license="BSD",
-        long_description=readme,
-        packages=packages,
-        package_data={
-            'jip.scripts': ['*.jip']
-        },
-        install_requires=["sqlalchemy==0.8.2",
-                          "jinja2==2.7",
-                          "argparse"
-                          ],
-        ext_modules=[dispatcher_ext],
-        entry_points={
-            "console_scripts": [
-                'jip = jip.cli.jip_main:main'
-            ]
-        }
-    )
-
-
-def rtd_install():
-    from distutils.core import setup, Extension
-
-    dispatcher_ext = Extension('jip.dispatcher',
-                               ['jip/dispatcher/jip_binding.c',
-                                'jip/dispatcher/jip_dispatcher.c'])
-    setup(
-        name=name,
-        version=version,
-        description=description,
-        author_email=author_email,
-        url=url,
-        license="BSD",
-        long_description=readme,
-        packages=packages,
-        package_data={
-            'jip.scripts': ['*.jip']
-        },
-        ext_modules=[dispatcher_ext],
-    )
-
-if on_rtd:
-    rtd_install()
-else:
-    default_install()
+dispatcher_ext = Extension('jip.dispatcher',
+                           ['jip/dispatcher/jip_binding.c',
+                            'jip/dispatcher/jip_dispatcher.c'])
+setup(
+    name=name,
+    version=version,
+    description=description,
+    author_email=author_email,
+    url=url,
+    license="BSD",
+    long_description=readme,
+    packages=packages,
+    package_data={
+        'jip.scripts': ['*.jip']
+    },
+    install_requires=["sqlalchemy==0.8.2",
+                      "jinja2==2.7",
+                      "argparse"
+                      ],
+    ext_modules=[dispatcher_ext],
+    entry_points={
+        "console_scripts": [
+            'jip = jip.cli.jip_main:main'
+        ]
+    }
+)
