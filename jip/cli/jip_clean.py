cf7aa3288cb497f52086eae14652d28cc278cda6
Thasso Griebel <thasso.griebel@googlemail.com>
2013-12-09 18:45:59 +0100

    Extended clean to also delete job data (#3)

--- jip/cli/jip_clean.py
+++ jip/cli/jip_clean.py
@@ -2,13 +2,19 @@
 """
 Clean jip jobs
 
+This command can be used to remove job log files and job data.
+
 Usage:
-    jip-clean [-j <id>...] [-J <cid>...]
+    jip-clean [-j <id>...] [-J <cid>...] [-l] [-d] [--dry]
     jip-clean [--help|-h]
 
 Options:
     -j, --job <id>           List jobs with specified id
     -J, --cluster-job <cid>  List jobs with specified cluster id
+    -l, --logs               Remove log files
+    -d, --data               Remove job data
+    --dry                    Dry run, print status but do not delete
+                             any files
     -h --help                Show this help message
 """
 import os
@@ -16,7 +22,7 @@ import os
 import jip.db
 import jip.jobs
 import jip.cluster
-from . import parse_args, parse_job_ids, confirm
+from . import parse_args, parse_job_ids, confirm, colorize, RED
 
 
 def main():
@@ -25,18 +31,48 @@ def main():
     jobs = jip.db.query(job_ids=job_ids, cluster_ids=cluster_ids,
                         archived=None)
     jobs = jip.jobs.resolve_jobs(jobs)
+    logs = args['--logs']
+    data = args['--data']
+    dry = args['--dry']
+
+    if not logs and not data:
+        print colorize("Neither --logs nor --data selected."
+                       "\nNothing to delete", RED)
+        return
+
+    if data and not confirm("You are about to delete job output!\n" +
+                            colorize(
+                                "The will remove result files from disk!\n",
+                                RED
+                            ) +
+                            "Are you sure ?"):
+        return
     if confirm("Are you sure you want "
-               "to delete %d jobs" % len(jobs),
+               "to clean %d jobs" % len(jobs),
                False):
         cluster = jip.cluster.get()
         for job in jobs:
-            print "Removing logs for", job
-            stdout = cluster.resolve_log(job, job.stdout)
-            if stdout and os.path.exists(stdout):
-                os.remove(stdout)
-            stderr = cluster.resolve_log(job, job.stderr)
-            if stderr and os.path.exists(stderr):
-                os.remove(stderr)
+            if logs:
+                print "Removing logs for", job
+                stdout = cluster.resolve_log(job, job.stdout)
+                _remove(stdout, dry=dry)
+                stderr = cluster.resolve_log(job, job.stderr)
+                _remove(stderr, dry=dry)
+            if data:
+                print "Removing data for", job
+                files = job.get_output_files()
+                for f in files:
+                    _remove(f, dry=dry)
+
+
+def _remove(f, dry=True):
+    if not f or not os.path.exists(f):
+        return
+    if dry:
+        print f
+    else:
+        print "Removing", f
+        os.remove(f)
 
 
 if __name__ == "__main__":
