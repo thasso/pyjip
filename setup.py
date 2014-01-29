9ecf5996f1d0567b1d86bcc678cbee5fa4ce8f8f
Thasso Griebel <thasso.griebel@googlemail.com>
2014-01-29 14:15:13 +0100

    Bumped version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except:
     from setuptools import setup, Extension
 
 name = 'pyjip'
-version = '0.5'
+version = '0.6'
 description = 'JIP pipeline library'
 author_email = "thasso.griebel@gmail.com"
 url = ""
