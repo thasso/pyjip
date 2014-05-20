1e8a706aadd0e78ce0c22f4119077fa510a0b4a6
Emilio Palumbo <emiliopalumbo@gmail.com>
2014-05-20 17:41:28 +0200

    Update connection string regexp

--- jip/db.py
+++ jip/db.py
@@ -614,7 +614,7 @@ def init(path=None, in_memory=False):
 
     # parse connection string
     import re
-    conn_re = re.compile(r"(?P<type>\w+)://(?:(?P<user>\w+):(?P<password>\w+)@)?(?P<host>\w+)?/(?P<db>.+)")
+    conn_re = re.compile(r"(?P<type>\w+)://(?:(?P<user>\w+):(?P<password>\w+)@)?(?P<host>[\w.]+)?/(?P<db>.+)")
     path_match = conn_re.match(path)
     if not path_match:
         ## dynamically create an sqlite path
